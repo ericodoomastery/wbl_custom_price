bc427af74cee9f28e2fa5e343313b1d0e6f7d756
ericbkotze <ericbkotze@gmail.com>
2026-03-11 10:15:23 +0200

    1

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
 # You can`t redistribute it and/or modify it.
 ##################################################################################
 {
-    'name': 'Custom Price',
+    'name': 'Custom Price 1',
     'version': '19.0.1.0.0',
     'description': """website custom pricing, dynamic pricing module,  minimum price control, flexible pricing app, negotiable price module, customer defined price, donation product module, user input price,  percentage pricing,  fixed minimum price, ecommerce price control, website price validation, sales order custom price, product variant pricing, dynamic price website, pricing flexibility, custom amount product, pricing management module, website sales integration, custom checkout price, configurable product price, frontend price input,  minimum order price control, ecommerce pricing solution, adjustable product price, price override with limit, controlled discount system.""",
     'summary': """website custom pricing, dynamic pricing module,  minimum price control, flexible pricing app, negotiable price module, customer defined price, donation product module, user input price,  percentage pricing,  fixed minimum price, ecommerce price control, website price validation, sales order custom price, product variant pricing, dynamic price website, pricing flexibility, custom amount product, pricing management module, website sales integration, custom checkout price, configurable product price, frontend price input,  minimum order price control, ecommerce pricing solution, adjustable product price, price override with limit, controlled discount system.""",
